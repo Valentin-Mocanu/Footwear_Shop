39b34c3b0a7be0b4276911efab4d90046e600fc5
ValentinV220 <valentinmocanu2004@gmail.com>
2026-07-17 19:19:48 +0300

    Translate whole source code in EN

---
+++ Footwear_Shop/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Footwear_Shop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxProducts = new System.Windows.Forms.ListBox();
+            this.buttonSell = new System.Windows.Forms.Button();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.labelProductList = new System.Windows.Forms.Label();
+            this.buttonLoadData = new System.Windows.Forms.Button();
+            this.buttonSaveData = new System.Windows.Forms.Button();
+            this.numericQuantity = new System.Windows.Forms.NumericUpDown();
+            this.labelSelectedProductss = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericQuantity)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBoxProducts
+            // 
+            this.listBoxProducts.FormattingEnabled = true;
+            this.listBoxProducts.ItemHeight = 16;
+            this.listBoxProducts.Location = new System.Drawing.Point(45, 77);
+            this.listBoxProducts.Name = "listBoxProducts";
+            this.listBoxProducts.Size = new System.Drawing.Size(340, 324);
+            this.listBoxProducts.TabIndex = 1;
+            // 
+            // buttonSell
+            // 
+            this.buttonSell.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.buttonSell.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSell.Location = new System.Drawing.Point(455, 300);
+            this.buttonSell.Name = "buttonSell";
+            this.buttonSell.Size = new System.Drawing.Size(116, 32);
+            this.buttonSell.TabIndex = 2;
+            this.buttonSell.Text = "Sell";
+            this.buttonSell.UseVisualStyleBackColor = false;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.buttonAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAdd.Location = new System.Drawing.Point(455, 353);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(116, 32);
+            this.buttonAdd.TabIndex = 3;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = false;
+            // 
+            // labelProductList
+            // 
+            this.labelProductList.AutoSize = true;
+            this.labelProductList.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelProductList.Location = new System.Drawing.Point(42, 32);
+            this.labelProductList.Name = "labelProductList";
+            this.labelProductList.Size = new System.Drawing.Size(99, 18);
+            this.labelProductList.TabIndex = 4;
+            this.labelProductList.Text = "Product list:";
+            // 
+            // buttonLoadData
+            // 
+            this.buttonLoadData.BackColor = System.Drawing.Color.Lime;
+            this.buttonLoadData.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonLoadData.Location = new System.Drawing.Point(455, 42);
+            this.buttonLoadData.Name = "buttonLoadData";
+            this.buttonLoadData.Size = new System.Drawing.Size(116, 32);
+            this.buttonLoadData.TabIndex = 5;
+            this.buttonLoadData.Text = "Load Stock";
+            this.buttonLoadData.UseVisualStyleBackColor = false;
+            // 
+            // buttonSaveData
+            // 
+            this.buttonSaveData.BackColor = System.Drawing.Color.Yellow;
+            this.buttonSaveData.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSaveData.Location = new System.Drawing.Point(455, 97);
+            this.buttonSaveData.Name = "buttonSaveData";
+            this.buttonSaveData.Size = new System.Drawing.Size(116, 32);
+            this.buttonSaveData.TabIndex = 6;
+            this.buttonSaveData.Text = "Save Stock";
+            this.buttonSaveData.UseVisualStyleBackColor = false;
+            // 
+            // numericQuantity
+            // 
+            this.numericQuantity.Location = new System.Drawing.Point(455, 244);
+            this.numericQuantity.Name = "numericQuantity";
+            this.numericQuantity.Size = new System.Drawing.Size(120, 22);
+            this.numericQuantity.TabIndex = 7;
+            // 
+            // labelSelectedProductss
+            // 
+            this.labelSelectedProductss.AutoSize = true;
+            this.labelSelectedProductss.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSelectedProductss.Location = new System.Drawing.Point(452, 211);
+            this.labelSelectedProductss.Name = "labelSelectedProductss";
+            this.labelSelectedProductss.Size = new System.Drawing.Size(203, 18);
+            this.labelSelectedProductss.TabIndex = 9;
+            this.labelSelectedProductss.Text = "Number of selected products:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(694, 450);
+            this.Controls.Add(this.labelSelectedProductss);
+            this.Controls.Add(this.numericQuantity);
+            this.Controls.Add(this.buttonSaveData);
+            this.Controls.Add(this.buttonLoadData);
+            this.Controls.Add(this.labelProductList);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.buttonSell);
+            this.Controls.Add(this.listBoxProducts);
+            this.Name = "Form1";
+            this.Text = "Footwear Shop";
+            ((System.ComponentModel.ISupportInitialize)(this.numericQuantity)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox listBoxProducts;
+        private System.Windows.Forms.Button buttonSell;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Label labelProductList;
+        private System.Windows.Forms.Button buttonLoadData;
+        private System.Windows.Forms.Button buttonSaveData;
+        private System.Windows.Forms.NumericUpDown numericQuantity;
+        private System.Windows.Forms.Label labelSelectedProductss;
+    }
+}
+
---
+++ Footwear_Shop/Form1.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Footwear_Shop
+{
+    public partial class Form1 : Form
+    {
+        // ----- COLLECTIONS -----
+        // Product list
+        private List<StockProduct> products = new List<StockProduct>();
+        private string stockFile = "stock.txt";
+
+        public Form1()
+        {
+            InitializeComponent();
+            buttonSell.Click += ButtonSell_Click;
+            buttonAdd.Click += ButtonAdd_Click;
+            buttonLoadData.Click += ButtonLoadData_Click;
+            buttonSaveData.Click += ButtonSaveData_Click;
+
+            // Load data from file if it exists
+            if (File.Exists(stockFile))
+            {
+                LoadStock();
+            }
+
+            RefreshProductList();
+        }
+
+        // Display products in the ListBox
+        private void RefreshProductList()
+        {
+            listBoxProducts.Items.Clear();
+            foreach (var product in products)
+            {
+                listBoxProducts.Items.Add(product.GetDescription());
+            }
+        }
+
+        // ----- LAMBDA EXPRESSIONS -----
+        // Products with low stock
+        private List<StockProduct> GetLowStockProducts()
+        {
+            return products
+                .Where(product => product.Quantity < 5)
+                .ToList();
+        }
+
+        // Sell products
+        private void ButtonSell_Click(object sender, EventArgs e)
+        {
+            if (listBoxProducts.SelectedIndex < 0)
+            {
+                MessageBox.Show("Please select a product before selling!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (numericQuantity.Value <= 0)
+            {
+                MessageBox.Show("The stock must be greater than 0!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                products[listBoxProducts.SelectedIndex]
+                    .Sell((int)numericQuantity.Value);
+
+                RefreshProductList();
+            }
+            catch (StockException ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Add stock
+        private void ButtonAdd_Click(object sender, EventArgs e)
+        {
+            if (listBoxProducts.SelectedIndex < 0)
+            {
+                MessageBox.Show("Please select a product before restocking!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (numericQuantity.Value <= 0)
+            {
+                MessageBox.Show("The stock must be greater than 0!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            products[listBoxProducts.SelectedIndex]
+                .Add((int)numericQuantity.Value);
+
+            RefreshProductList();
+        }
+
+        // Save products to the text file
+        private void ButtonSaveData_Click(object sender, EventArgs e)
+        {
+
+            if (!File.Exists(stockFile))
+            {
+                MessageBox.Show("The stock file doesn't exist!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            using (StreamWriter writer = new StreamWriter(stockFile))
+            {
+                foreach (var product in products)
+                {
+                    // Write each product on a separate line
+                    writer.WriteLine($"{product.Name};{product.Brand};{product.Price};{product.Size};{product.Quantity}");
+                }
+            }
+            MessageBox.Show("The product list has been successfully saved!", "Save", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        // Load products from the text file
+        private void ButtonLoadData_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(stockFile))
+            {
+                MessageBox.Show("The stock file doesn't exist!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            bool loaded = LoadStock();
+
+            if (loaded)
+            {
+                RefreshProductList();
+
+                MessageBox.Show("The product list has been successfully loaded!", "Load", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                var lowStockProducts = GetLowStockProducts();
+                if (lowStockProducts.Count > 0)
+                {
+                    MessageBox.Show($"Low stock! {lowStockProducts.Count} product(s) have low stock!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+        }
+
+        // Load products from the file
+        private bool LoadStock()
+        {
+            products.Clear();
+
+            try
+            {
+                foreach (var line in File.ReadAllLines(stockFile))
+                {
+                    if (string.IsNullOrWhiteSpace(line))
+                        continue;
+
+                    var parts = line.Split(';');
+
+                    if (parts.Length != 5)
+                        throw new Exception("Invalid product format!");
+
+                    string name = parts[0];
+                    string brand = parts[1];
+
+                    if (!double.TryParse(parts[2], out double price))
+                        throw new Exception("Invalid price in stock file!");
+
+                    if (!int.TryParse(parts[3], out int size))
+                        throw new Exception("Invalid size in stock file!");
+
+                    if (!int.TryParse(parts[4], out int quantity))
+                        throw new Exception("Invalid quantity in stock file!");
+
+                    if (quantity < 0)
+                        throw new Exception("The quantity can't be a negative number!");
+
+                    products.Add(new StockProduct(name, brand, price, size, quantity));
+                }
+                return true;
+            }
+
+            catch (Exception ex)
+            {
+                products.Clear();
+                MessageBox.Show("Error reading the stock file:\n\n" + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                return false;
+            }
+        }
+
+        // ----- EXCEPTION HANDLING -----
+        // Custom exception
+        public class StockException : Exception
+        {
+            public StockException(string message) : base(message) { }
+        }
+
+        // ----- INTERFACES -----
+        public interface IStock
+        {
+            void Add(int quantity);
+            void Sell(int quantity);
+        }
+
+        // ----- ABSTRACT CLASS -----
+        public abstract class Product
+        {
+            public string Name { get; set; }
+            public string Brand { get; set; }
+            public double Price { get; set; }
+
+            protected Product(string name, string brand, double price)
+            {
+                Name = name;
+                Brand = brand;
+                Price = price;
+            }
+
+            public abstract string GetDescription();
+        }
+
+        public class Footwear : Product
+        {
+            public int Size { get; set; }
+
+            public Footwear(string name, string brand, double price, int size)
+                : base(name, brand, price)
+            {
+                Size = size;
+            }
+
+            public override string GetDescription()
+            {
+                return $"{Name} | {Brand} | {Size} | {Price} RON";
+            }
+        }
+
+        public class StockProduct : Footwear, IStock
+        {
+            public int Quantity { get; private set; }
+
+            public StockProduct(string name, string brand, double price, int size, int quantity)
+                : base(name, brand, price, size)
+            {
+                Quantity = quantity;
+            }
+
+            public override string GetDescription()
+            {
+                return $"{Name} | {Brand} | {Size} | {Price} RON | Stock: {Quantity}";
+            }
+
+            public void Add(int quantity)
+            {
+                Quantity += quantity;
+            }
+
+            public void Sell(int quantity)
+            {
+                if (quantity > Quantity)
+                    throw new StockException("Insufficient stock!");
+
+                Quantity -= quantity;
+            }
+        }
+    }
+}
