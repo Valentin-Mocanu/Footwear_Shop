4bb0af3ae4b7fb0b457161a772a1ecaad67a1785
ValentinV220 <valentinmocanu2004@gmail.com>
2026-07-20 21:11:32 +0300

    Add app icon

--- Footwear_Shop/Form1.Designer.cs
+++ Footwear_Shop/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.listBoxProducts = new System.Windows.Forms.ListBox();
             this.buttonSell = new System.Windows.Forms.Button();
             this.buttonAdd = new System.Windows.Forms.Button();
@@ -133,6 +134,7 @@
             this.Controls.Add(this.buttonAdd);
             this.Controls.Add(this.buttonSell);
             this.Controls.Add(this.listBoxProducts);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Footwear Shop";
             ((System.ComponentModel.ISupportInitialize)(this.numericQuantity)).EndInit();
